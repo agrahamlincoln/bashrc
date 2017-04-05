fb2b5db4a858c43e9267e9460726790983c342c1
Graham Rounds <graham@platform9.com>
2017-04-05 13:13:39 -0700

    Add lsn alias

--- init.sh
+++ init.sh
@@ -8,6 +8,8 @@ alias ..="cd .."
 alias ll="ls -alF"
 alias la="ls -A"
 alias l="ls -CF"
+# ls -l and show numeric unix permissions (i.e. 600)
+alias lsn="ls -l | awk '{k=0;for(i=0;i<=8;i++)k+=((substr(\$1,i+2,1)~/[rwx]/) *2^(8-i));if(k)printf(\"%0o \",k);print}'"
 alias brc="vim ~/src/bashrc/init.sh"
 alias brcl="vim ~/src/bashrc/init-local.sh"
 alias src="source ~/.bashrc"
